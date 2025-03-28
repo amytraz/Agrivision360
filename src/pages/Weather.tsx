82b9cfcabda663e74a01410b8dcb4ad5d6c23491
AMIT RAJ THAKUR <143288096+amytraz@users.noreply.github.com>
2025-03-28 09:36:17 +0530

    Update Weather.tsx

--- src/pages/Weather.tsx
+++ src/pages/Weather.tsx
@@ -1,6 +1,6 @@
 
 import React, { useState, useEffect } from 'react';
-import Navbar from '@/components/Navbar';
+import DashboardNavbar from '@/components/DashboardNavbar';
 import { 
   CloudRain, 
   Droplets, 
